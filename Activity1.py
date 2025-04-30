b3b9d27a8fce85c94ca0f8b8dd84a0606b950277
anishas07 <Studytea95@gmail.com>
2025-04-30 20:25:05 +0930

    Add files via upload

---
+++ Activity1.py
@@ -0,0 +1,10 @@
+def printnumber(n):
+    iteration=0
+    print("The number used by the user", n)
+    iteration+=1
+    print("The total numbers of iterations done by the code is", iteration,"\n")
+
+printnumber(5)
+printnumber(10)
+print("\n With any n the time taken by the code will not change.")
+    
